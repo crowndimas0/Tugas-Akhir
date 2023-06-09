4745a9b934b6e2bb12e36befa5e4bbe08baa5cd2
Dimas <92238113+crowndimas0@users.noreply.github.com>
2023-06-09 19:57:44 +0700

    Create Movie of CDOD by Ls (MY24).py
    MY24 not indicating as global dust storm

---
+++ Movie of CDOD by Ls (MY24).py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+# Load data into dataframe
+df = pd.read_csv('/content/drive/MyDrive/Dimas_Draft_Praproposal/Hasil/temporal analysis/dust_MY24.csv')
+
+cmap = 'jet'
+vmin = 0
+vmax = 2
+
+for i, Ls in enumerate(df['Ls'].unique()):
+  df_ls = df[df['Ls'] == Ls]
+
+  fig, ax = plt.subplots(figsize=(8, 6))
+  sc = ax.scatter(df_ls['longitude'], df_ls['latitude'], c=df_ls['cdod'], cmap=cmap, vmin=vmin, vmax=vmax)
+  ax.set_xlabel('Longitude', fontsize=12)
+  ax.set_ylabel('Latitude', fontsize=12)
+  ax.set_xticks([-180, 0, 180])
+  ax.set_yticks([-45, 0, 45])
+  ax.tick_params(axis='both', which='major', labelsize=8)
+  ax.set_title('Ls = {}'.format(Ls), fontsize=10)
+  plt.savefig("MY24_cdod_Ls_"+str(i)+".png", dpi=150)
+  plt.close()
+
+  import moviepy.video.io.ImageSequenceClip
+
+images = []
+for i in range(669):
+  filename = "MY24_cdod_Ls_"+str(i)+".png"
+  images.append(filename)
+
+fps = 60
+
+clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(images, fps=fps)
+clip.write_videofile('MY24.mp4')
